df9c0851be7b7033c78c2717f873abca7d0c25d0
Rodrigo R. Rubio Haro <chavo0022009@gmail.com>
2019-05-06 15:59:43 -0500

    07 Se agrego la base para sobrecarga de metodos

---
+++ 07.SobrecargaMetodos/Main.java
@@ -0,0 +1,27 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public String metodo() {
+        System.out.println("Metodo1");
+        return "hola";
+
+    }
+
+    public String metodo(char dato) {
+        System.out.println("Metodo2");
+        return '3';
+    }
+
+    public String metodo(float dato) {
+        System.out.println("Metodo3");
+        return 3.1416;
+    }
+
+    public String metodo(int dato) {
+        System.out.println("Metodo4");
+        return 1;
+    }
+
+}
