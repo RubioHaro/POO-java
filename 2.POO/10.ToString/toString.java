6c86844c60a3dc097f77d258323195be2135dc72
Rodrigo R. Rubio Haro <chavo0022009@gmail.com>
2019-12-29 19:04:29 -0600

    23. Java
    Se agregó el método toString

---
+++ 2.POO/10.ToString/toString.java
@@ -0,0 +1,105 @@
+
+/**
+ * toString
+ */
+public class toString {
+
+    public static void main(final String[] args) {
+        Persona Roy = new Persona();
+        Roy.setNombre("Rodrigo");
+        System.out.println(Roy);
+        System.out.println(Roy.toString());
+    }
+
+}
+
+class Persona {
+
+    private String nombre;
+    private String apellido_paterno;
+    private String apellido_materno;
+    private String raza;
+    public int edad;
+
+    public Persona() {
+
+    }
+
+    public boolean comer(int x) {
+        if (x > 0)
+            return true;
+        else
+            return false;
+
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido_paterno
+     */
+    public String getApellido_paterno() {
+        return apellido_paterno;
+    }
+
+    /**
+     * @param apellido_paterno the apellido_paterno to set
+     */
+    public void setApellido_paterno(String apellido_paterno) {
+        this.apellido_paterno = apellido_paterno;
+    }
+
+    /**
+     * @return the apellido_materno
+     */
+    public String getApellido_materno() {
+        return apellido_materno;
+    }
+
+    /**
+     * @param apellido_materno the apellido_materno to set
+     */
+    public void setApellido_materno(String apellido_materno) {
+        this.apellido_materno = apellido_materno;
+    }
+
+    /**
+     * @return the raza
+     */
+    public String getRaza() {
+        return raza;
+    }
+
+    /**
+     * @param raza the raza to set
+     */
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    /**
+     * @return the full name
+     */
+    public String getNombreCompleto() {
+        return nombre + " " + apellido_paterno + " " + apellido_materno;
+    }
+
+    @Override
+    public String toString() {
+        
+        return "mi nombre es " + this.getNombre() + ".";
+    }
+
+}
